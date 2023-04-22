10ea4bd43807e4bd2fd112a26e347915109d31f6
marilynel <marilyneleary@gmail.com>
2023-04-21 17:19:51 -0700

    making it prettier and also finding more bugs

--- DIGITfiles/FilterFasta.py
+++ DIGITfiles/FilterFasta.py
@@ -1,14 +1,54 @@
+'''
+FilterFasta.py finds multiple genomic sequences in a genome at given coordinates.
+
+Written by: Marilyn Leary 2023
+
+    Input:
+    filename        Path to fasta file of reference genome (many can be found at
+                    https://www.ncbi.nlm.nih.gov/data-hub/genome/)
+    coordinates     A dictionary containing the information to look up in reference genome.
+    Format as follows:
+
+                    coordinates = {
+                        alleleName : [
+                            chromosomeID, [startingPosition, endingPosition]
+                        ],
+                        "R01C02_wt" : [
+                            "chr5", [66710080, 66714080]
+                        ]
+                    }
+
+                    As many allele coordinate sets as needed may be listed in this format.
+
+    Output:
+    sequenceData    Dictionary containing sequences for each allele listed in coordinates. Format
+    as follows:
+
+                    sequenceData = {
+                        alleleName : sequence01
+                        "R01C02_wt" :
+                        "TCAAGGGCACCAGCCGTCCTGCTCACTACCATGTCTTGTGGGACGAGAACAACTTCACAGCCGACGCACTGCAGACCCTCA
+                                        CAACAACCTTTGCTACACGTAAGCTAGCTGCTCACAAAAAGAGGTGTCAGTGTCAGTTCAGTTCCCTGAACACCGACCGT
+                                        TAATATAATAGCTTGTCAAATTGCCGCTGCAGCTACGCGAGGTGCACGCGCTCTGTGTCCATTGGTAGGTTTGTCAAAGT
+                                        TATCCATTGCTGAATCGATGCACGAGCAACAGATAAACCAAAAGATGCATATGCCTTCCTTGCAGTCCCGCCGGCGTACT
+                                        GCTCACCTGGCCGCATTCCGCGCCCGGTTCTACATGGAGCCTGACAGCTCAGACAGCGGGTCGCTGGCGAGTGGCGCCT"
+                    }
+
+                    All of the alleles given in the coordinate set will be in this returned struct.
+
+'''
+
 import sys
 
 
 class FilterFasta:
     def __init__(self, coordinates):
-        self.dictAllCoordinates = {}  # keys = chromosome, values = list of all coordinates
-        # needed on that chromosome
-        self.dictMinCoordinates = {}  # keys = chromosome, values =
-        self.dictMaxCoordinates = {}
-        self.genomeStruct = {}
-        self.listChr = []
+        self.dictAllCoordinates = {}  # keys = chr, values = list of all coordinates needed on
+        # that chromosome
+        self.dictMinCoordinates = {}  # TODO: structure?
+        self.dictMaxCoordinates = {}  # TODO: structure?
+        self.genomeStruct = {}  # TODO: structure?
+        self.listChr = []  # TODO: structure?
 
         self.__setVals__(coordinates)
         self.__setMinMax__()
@@ -25,8 +65,9 @@ class FilterFasta:
 
     def __setMinMax__(self):
         '''
-        Method finds minum and maximum coordinates for each chromosome. Used to limit how much
-        data is pulled from genome file.
+        Finds minimum and maximum coordinates for each chromosome. Used to limit how much data is
+        pulled from genome
+        file.
         '''
         for chr in self.dictAllCoordinates:
             self.dictMinCoordinates[chr] = min(self.dictAllCoordinates[chr]) - 1
@@ -43,7 +84,7 @@ def filterFasta(filename, coordinates):
                 if chrSeq and (key in ffobj.listChr):
                     ffobj.genomeStruct[key] = chrSeq[ffobj.dictMinCoordinates[key]:
                                                      ffobj.dictMaxCoordinates[key]]
-                    chrSeq, key = "", ""
+                chrSeq, key = "", ""
                 key = line[1:].strip()
             else:
                 chrSeq += line.strip()
@@ -51,9 +92,8 @@ def filterFasta(filename, coordinates):
     sequenceData = {}
     for c in coordinates:
         chr = coordinates[c][0]
-        # c1 - 1 is to keep results consistent with other version of filterfasta
-        c1, c2 = coordinates[c][1][0] - ffobj.dictMinCoordinates[chr] - 1, coordinates[c][1][1] - \
-                 ffobj.dictMinCoordinates[chr]
+        c1 = coordinates[c][1][0] - ffobj.dictMinCoordinates[chr] - 1
+        c2 = coordinates[c][1][1] - ffobj.dictMinCoordinates[chr]
         seq = ffobj.genomeStruct[chr][c1:c2]
         sequenceData[c] = seq
 
